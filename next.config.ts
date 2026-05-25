6d2c345d8de4055efb42e2911173e68fae4efd69
kaifgowani <kaifgowani@gmail.com>
2026-05-25 23:53:40 +0530

    fixed nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.watchOptions = {
+      ignored: /functions/,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
